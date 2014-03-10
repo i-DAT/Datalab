741a8e0ae4fca49796e3079e14860e7e0a3ced60
thisisthechris <mail@thisisthechris.co.uk>
2014-03-10 12:10:16 +0000

    Adds filtering and Excel export

--- perception/admin.py
+++ perception/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from models import *
 
 admin.site.register(Camera)
-admin.site.register(Volume)
-admin.site.register(Motion)
+
+
+class MotionAdmin(admin.ModelAdmin):
+    list_filter = ['camera']
+
+class VolumeAdmin(admin.ModelAdmin):
+    list_filter = ['camera']
+
+admin.site.register(Volume, VolumeAdmin)
+admin.site.register(Motion, MotionAdmin)
+
+
+
+class MyAdmin(admin.ModelAdmin):
+    actions = [export_as_xls]
+
+admin.site.add_action(export_as_xls)
